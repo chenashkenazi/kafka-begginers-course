b0ec2eb9690a623e3efb413f96fd177dbb9e9a84
chenashkenazi <chenashkenazi95@gmail.com>
2022-10-21 17:21:35 +0300

    continue

--- kafka-basics/src/main/java/org/example/demos/kafka/ProducerDemoKeys.java
+++ kafka-basics/src/main/java/org/example/demos/kafka/ProducerDemoKeys.java
@@ -7,9 +7,9 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemoWithCallback {
+public class ProducerDemoKeys {
 
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
 
     public static void main(String[] args) {
         log.info("Producing!");
@@ -26,9 +26,13 @@ public class ProducerDemoWithCallback {
 
         for (int i = 0; i < 10; i++) {
 
+            String topic = "demo_java";
+            String value = "hello world " + i;
+            String key = "id_" + i;
+
             // create producer record
             ProducerRecord<String, String> producerRecord =
-                    new ProducerRecord<>("demo_java", "hello world " + i);
+                    new ProducerRecord<>(topic, key, value);
 
             // send data - asynchronous
             producer.send(producerRecord, new Callback() {
@@ -39,6 +43,7 @@ public class ProducerDemoWithCallback {
                         // record successfully sent
                         log.info("Received new metadata/ \n" +
                                 "Topic: " + metadata.topic() + "\n" +
+                                "key: " + producerRecord.key() + "\n" +
                                 "Partition: " + metadata.partition() + "\n" +
                                 "Offset: " + metadata.offset() + "\n" +
                                 "Timestamp: " + metadata.timestamp());
@@ -47,15 +52,7 @@ public class ProducerDemoWithCallback {
                     }
                 }
             });
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
         }
-
-
 
         // flush data - synchronous
         // stop here until all data sent
